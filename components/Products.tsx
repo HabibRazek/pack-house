a32613fb608699de4c3b8dcd385e6fc52be1cad2
HabibRazek <habibrazeg32@gmail.com>
2024-08-08 12:12:42 +0100

    img-fix

--- components/Products.tsx
+++ components/Products.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Product } from '../types';
+import Image from 'next/image';
 
 interface ProductsProps {
   products: Product[];
@@ -16,7 +17,7 @@ const Products: React.FC<ProductsProps> = ({ products }) => {
           <div key={product.id}>
             <h2>{product.name}</h2>
             <div dangerouslySetInnerHTML={{ __html: product.description }}></div>
-            {product.image && <img src={product.image.sourceUrl} alt={product.name} />}
+            {product.image && <Image  src={product.image.sourceUrl} alt={product.name} width={500} height={500} />}
             <div>{product.price}</div>
           </div>
         ))}
